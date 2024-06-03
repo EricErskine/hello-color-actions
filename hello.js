34717c1091238507ff3e770eac962dac7601e316
Eric Erskine <eerskine@msinteractive.com>
2024-06-03 14:21:52 -0400

    update alert message

--- hello.js
+++ hello.js
@@ -1,4 +1,4 @@
-const hello = () => "Hello World from the Office Hours in the terminal!";
+const hello = () => "Hello World from Eric's repo for module 4 CI/CD!";
 console.log(hello());
 exports.hello = hello;
 
